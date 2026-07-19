a34045d7f350a8fcd295e6cbbac74035df092358
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2026-07-19 07:13:38 -0300

    feat(api): actualizar la respuesta de la raíz para incluir detalles de la API y estado

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
-        "message" => "Sistema contable - API",
-        "version" => "Beta"
+        "name" => "API del sistema contable de Costa Tech",
+        "version" => "v1.0.0-beta",
+        "message" => "Esta es una API privada para el sistema contable. El acceso a los recursos requiere autenticación.",
+        "environment" => app()->environment(),
+        "meta" => [
+            "timestamp" => now()->toIso8601String(),
+            "status" => "online",
+        ]
     ]);
 });
 
